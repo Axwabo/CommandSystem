23a26d043ea5a71a526285114e3f292d61bdc249
Axwabo <axwabo@gmail.com>
2023-01-18 09:05:42 +0100

    init plugin

---
+++ Axwabo.CommandSystem/Plugin.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using HarmonyLib;
+using PluginAPI.Core;
+using PluginAPI.Core.Attributes;
+
+namespace Axwabo.CommandSystem {
+
+    public sealed class Plugin {
+
+        public static Plugin Instance { get; private set; }
+
+        private Harmony _harmony;
+
+        [PluginEntryPoint("Axwabo.CommandSystem", "1.0.0", "Adds a sophisticated command system to the game.", "Axwabo")]
+        private void OnEnable() {
+            Instance = this;
+            _harmony = new Harmony("Axwabo.CommandSystem");
+            try {
+                _harmony.PatchAll();
+            } catch (Exception e) {
+                Log.Error("Patching failed! Some features will not work properly.\n" + e);
+            }
+        }
+
+        [PluginUnload]
+        private void OnDisable() {
+            Instance = null;
+            _harmony.UnpatchAll();
+        }
+
+    }
+
+}
