d2b83c373d547e76d929b96cdc0f090f24aac2d8
Axwabo <axwabo@gmail.com>
2023-01-18 16:08:10 +0100

    base of registration process

---
+++ Axwabo.CommandSystem/Registration/CommandRegistrationProcessor.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Reflection;
+
+namespace Axwabo.CommandSystem.Registration {
+
+    public sealed class CommandRegistrationProcessor {
+
+        public static void RegisterAll(object assemblyMemberInstance) => RegisterAll(assemblyMemberInstance.GetType());
+
+        public static void RegisterAll(Type typeInAssembly)
+            => new CommandRegistrationProcessor(typeInAssembly.Assembly)
+                .WithRegistrationAttributesFrom(typeInAssembly)
+                .Execute();
+
+        public static void RegisterAll(Assembly assembly) => new CommandRegistrationProcessor(assembly).Execute();
+
+        public static CommandRegistrationProcessor Create(object assemblyMemberInstance) => Create(assemblyMemberInstance.GetType().Assembly);
+
+        public static CommandRegistrationProcessor Create(Type typeInAssembly) => Create(typeInAssembly.Assembly);
+
+        public static CommandRegistrationProcessor Create(Assembly assembly) => new(assembly);
+
+        public Assembly TargetAssembly { get; }
+
+        private CommandRegistrationProcessor(Assembly assembly) => TargetAssembly = assembly;
+
+        public void Execute() {
+            // TODO: Implement
+        }
+
+    }
+
+}
---
+++ Axwabo.CommandSystem/Registration/RegistrationExtensions.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Axwabo.CommandSystem.Registration {
+
+    public static class RegistrationExtensions {
+
+        public static CommandRegistrationProcessor WithRegistrationAttributesFrom(this CommandRegistrationProcessor processor, Type type) {
+            // TODO: handle attributes
+            return processor;
+        }
+
+    }
+
+}
