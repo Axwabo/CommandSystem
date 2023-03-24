6ec5ccf34b6de226ee48f5a146cdaa5f86e38ea3
Axwabo <axwabo@gmail.com>
2023-03-24 22:35:56 +0100

    invert some config values

--- Axwabo.CommandSystem/ExiledConfig.cs
+++ Axwabo.CommandSystem/ExiledConfig.cs
@@ -16,16 +16,16 @@ public sealed class Config : IConfig
     public bool Debug { get; set; } = false;
 
     [Description("Whether playerID copying should be replaced with nickname copying.")]
-    public bool CopyNicknameInsteadOfId { get; set; } = true;
+    public bool CopyNicknameInsteadOfId { get; set; } = false;
 
     [Description("Whether Remote Admin extensions should be enabled.")]
     public bool EnableRemoteAdminExtensions { get; set; } = true;
 
     [Description("Whether to strip IL offsets from the exception stack trace.")]
-    public bool StripIntermediateLanguageOffsets { get; set; } = true;
+    public bool StripIntermediateLanguageOffsets { get; set; } = false;
 
     [Description("Whether to allow selecting the host player as a target using custom selectors.")]
-    public bool AllowSelectingHost { get; set; } = true;
+    public bool AllowSelectingHost { get; set; } = false;
 
 }
 
