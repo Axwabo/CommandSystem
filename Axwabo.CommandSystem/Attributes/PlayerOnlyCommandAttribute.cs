c995bf2b6055da60face53a4ce0eb90df7cf1b55
Axwabo <axwabo@gmail.com>
2023-08-23 12:34:22 +0200

    i forgor PlayerOnlyCommandAttribute.cs

--- Axwabo.CommandSystem/Attributes/PlayerOnlyCommandAttribute.cs
+++ Axwabo.CommandSystem/Attributes/PlayerOnlyCommandAttribute.cs
@@ -5,7 +5,7 @@ namespace Axwabo.CommandSystem.Attributes;
 /// <summary>
 /// Sets the command to be only usable by players. This attribute is inherited.
 /// </summary>
-[AttributeUsage(AttributeTargets.Class)]
+[AttributeUsage(AttributeTargets.Class | AttributeTargets.Method)]
 public sealed class PlayerOnlyCommandAttribute : Attribute, IPlayerOnlyAttribute
 {
 
