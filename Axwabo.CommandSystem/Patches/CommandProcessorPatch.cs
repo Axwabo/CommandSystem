420a2aadbc026f5690ffc21c12f801f5445e761b
Axwabo <axwabo@gmail.com>
2025-02-02 15:47:22 +0100

    fix CommandProcessorPatch

--- Axwabo.CommandSystem/Patches/CommandProcessorPatch.cs
+++ Axwabo.CommandSystem/Patches/CommandProcessorPatch.cs
@@ -1,4 +1,5 @@
-﻿using Axwabo.CommandSystem.Selectors;
+﻿using Axwabo.CommandSystem.Exceptions;
+using Axwabo.CommandSystem.Selectors;
 using HarmonyLib;
 using static Axwabo.CommandSystem.Selectors.PlayerSelectionManager;
 
@@ -8,49 +9,26 @@ namespace Axwabo.CommandSystem.Patches;
 internal static class CommandProcessorPatch
 {
 
-    public static IEnumerable<CodeInstruction> Transpiler(IEnumerable<CodeInstruction> instructions, ILGenerator generator)
+    public static IEnumerable<CodeInstruction> Transpiler(IEnumerable<CodeInstruction> instructions)
     {
         var list = new List<CodeInstruction>(instructions);
-
-        var emptyArrayFix = list.FindCode(OpCodes.Stloc_0) + 1;
-        var getCommandLabel = generator.DefineLabel();
-
-        // fixes the case where External Lookup is clicked while not selecting a target (string is empty)
-        list[emptyArrayFix].labels.Add(getCommandLabel);
-        list.InsertRange(emptyArrayFix, new[]
-        {
-            Ldloc(0),
-            Ldlen,
-            getCommandLabel.True(),
-            String("Command name was not specified!"),
-            Return
-        });
-
-        var pre = list.FindCode(OpCodes.Ldloc_1);
-        list.InsertRange(pre, new[]
-        {
-            This.MoveBlocksFrom(list[pre]),
-            Ldarg(1),
+        var setSender = list.FindCode(OpCodes.Starg_S);
+        list.InsertRange(setSender, [
+            Duplicate,
             Stfld(typeof(PlayerSelectionManager), nameof(CurrentSender))
-        });
+        ]);
 
         var failedIndex = list.FindIndex(i => i.operand is CommandExecutionFailedError);
-
-        var send = list.FindCall("ToUpperInvariant", failedIndex) - 6;
-        list.RemoveRange(send, 10);
-        list.InsertRange(send, new[]
-        {
+        var failEnd = list.FindCode(OpCodes.Stloc_S, start: failedIndex);
+        list.RemoveRange(failedIndex, failEnd - failedIndex);
+        list.InsertRange(failedIndex, [
+            Ldarg(1),
             Ldloc(0),
-            Int0,
-            LdelemRef,
-            Call<string>(nameof(string.ToUpperInvariant)),
-            String("#"),
-            Ldloc(12),
-            Call<string>(nameof(string.Concat), new[] {typeof(string), typeof(string), typeof(string)}),
-            Int0,
-            Int1,
-            String("")
-        });
+            Ldloc(11),
+            Call<DeveloperMode>(nameof(DeveloperMode.OnExceptionThrown)),
+            Ldloc(11),
+            Call<PlayerListProcessorException>(nameof(PlayerListProcessorException.CreateMessage)),
+        ]);
         return list;
     }
 
