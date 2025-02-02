b7e4f9227eba11f13365ebde0cba915e10d212d9
Axwabo <axwabo@gmail.com>
2025-02-02 15:51:52 +0100

    fix ConsolePatch

--- Axwabo.CommandSystem/Patches/ConsolePatch.cs
+++ Axwabo.CommandSystem/Patches/ConsolePatch.cs
@@ -1,6 +1,7 @@
 ﻿using Axwabo.CommandSystem.Exceptions;
 using Axwabo.CommandSystem.Selectors;
 using HarmonyLib;
+using LabApi.Events.Arguments.ServerEvents;
 using static Axwabo.CommandSystem.Selectors.PlayerSelectionManager;
 using Console = GameCore.Console;
 
@@ -13,39 +14,24 @@ internal static class ConsolePatch
     public static IEnumerable<CodeInstruction> Transpiler(IEnumerable<CodeInstruction> instructions)
     {
         var list = new List<CodeInstruction>(instructions);
-        var pre = list.FindCode(OpCodes.Ldloc_2);
+        var setSender = list.FindCall<CommandExecutingEventArgs>("get_Sender") + 1;
 
-        list.InsertRange(pre, new[]
-        {
-            This.MoveBlocksFrom(list[pre]),
-            Ldarg(2),
+        list.InsertRange(setSender, [
+            Duplicate,
             Stfld(typeof(PlayerSelectionManager), nameof(CurrentSender))
-        });
-        list.InsertRange(list.FindCode(OpCodes.Stloc_S, start: pre) + 1, new[]
-        {
-            Null,
-            Stfld(typeof(PlayerSelectionManager), nameof(CurrentSender)),
-            Ldarg(2),
-            Ldloc(1),
-            Ldloc(10),
-            Ldloc(9),
-            Call<DeveloperMode>(nameof(DeveloperMode.OnCommandExecuted))
-        });
+        ]);
 
         var failedIndex = list.FindIndex(i => i.operand is CommandExecutionFailedError);
-        list.RemoveRange(failedIndex, 6);
-        list.InsertRange(failedIndex, new[]
-        {
-            Null,
-            Stfld(typeof(PlayerSelectionManager), nameof(CurrentSender)),
+        var failEnd = list.FindCode(OpCodes.Stloc_S, start: failedIndex);
+        list.RemoveRange(failedIndex, failEnd - failedIndex);
+        list.InsertRange(failedIndex, [
             Ldarg(2),
             Ldloc(1),
-            Ldloc(12),
+            Ldloc(15),
             Call<DeveloperMode>(nameof(DeveloperMode.OnExceptionThrown)),
-            Ldloc(12),
+            Ldloc(15),
             Call<PlayerListProcessorException>(nameof(PlayerListProcessorException.CreateMessage)),
-            Stloc(13)
-        });
+        ]);
         return list;
     }
 
