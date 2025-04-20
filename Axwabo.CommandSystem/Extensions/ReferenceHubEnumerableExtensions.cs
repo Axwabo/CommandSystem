6442d4f19c49ff510d62043453173245fa3786bd
Axwabo <axwabo@gmail.com>
2025-04-20 19:00:52 +0200

    more ReferenceHubEnumerableExtensions

--- Axwabo.CommandSystem/Extensions/ReferenceHubEnumerableExtensions.cs
+++ Axwabo.CommandSystem/Extensions/ReferenceHubEnumerableExtensions.cs
@@ -1,4 +1,6 @@
-﻿namespace Axwabo.CommandSystem.Extensions;
+﻿using LabApi.Features.Wrappers;
+
+namespace Axwabo.CommandSystem.Extensions;
 
 /// <summary>Extension methods for <see cref="ReferenceHub"/> <see cref="IEnumerable{T}">enumerables</see>.</summary>
 public static class ReferenceHubEnumerableExtensions
@@ -12,5 +14,19 @@ public static class ReferenceHubEnumerableExtensions
     /// <returns>The combined string.</returns>
     public static string CombineNicknames(this IEnumerable<ReferenceHub> hubs, string separator = ", ")
         => string.Join(separator, hubs.Select(p => p.nicknameSync.MyNick));
+
+    /// <summary>
+    /// Maps each <see cref="ReferenceHub"/> of the enumerable to a LabApi <see cref="Player"/> wrapper.
+    /// </summary>
+    /// <param name="hubs">The enumerable to map.</param>
+    /// <returns>An <see cref="IEnumerable{T}"/> of LabApi <see cref="Player"/> wrappers.</returns>
+    public static IEnumerable<Player> ToPlayers(this IEnumerable<ReferenceHub> hubs) => hubs.Select(Player.Get);
+
+    /// <summary>
+    /// Maps and collects the <see cref="ReferenceHub"/> enumerable to a <see cref="List{T}"/> of LabApi <see cref="Player"/> wrappers.
+    /// </summary>
+    /// <param name="hubs">The enumerable to convert.</param>
+    /// <returns>A <see cref="List{T}"/> with each <see cref="ReferenceHub"/> mapped to a <see cref="Player"/>.</returns>
+    public static List<Player> ToPlayerList(this IEnumerable<ReferenceHub> hubs) => hubs.ToPlayers().ToList();
 
 }
